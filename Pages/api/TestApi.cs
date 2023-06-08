4506d6e1eaa111aaf206617788531a5c7e7916ae
DewDonut <n_alary@hetic.eu>
2023-06-08 11:37:55 +0200

    Ajoutez des fichiers projet.

---
+++ Pages/api/TestApi.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bragi.Pages.api
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestApi : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
